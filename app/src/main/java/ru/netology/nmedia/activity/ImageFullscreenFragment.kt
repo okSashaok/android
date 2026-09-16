a2f452e37bc136a646cc5cbeaa5c9a52d9905684
Александр <oksashaok@gmail.com>
2026-09-16 12:08:38 +0300

    Очистка от лишних строк

--- app/src/main/java/ru/netology/nmedia/activity/ImageFullscreenFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/ImageFullscreenFragment.kt
@@ -5,15 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import ru.netology.nmedia.databinding.FragmentImageFullscreenBinding
-import ru.netology.nmedia.viewmodel.PostViewModel
-import kotlin.getValue
 
 class ImageFullscreenFragment : Fragment() {
-    private val viewModel: PostViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
